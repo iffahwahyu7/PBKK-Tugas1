043a4f1a87a95fc6a915b0e69fa63a78d7413504
iffahwahyu7 <iffahwahyuroshifa@gmail.com>
2021-04-09 13:30:00 +0700

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        $customers = DB::table('sales.customers')->whereBetween('customer_id', [621, 640])->get();
+
+        return view('welcome', ['customers' => $customers]);
+    }
+}
